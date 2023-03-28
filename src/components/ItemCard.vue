c0c26995ec3d22fc9f9f927dffa3bc9f3caee6e9
SimoneFerrini <ferrini.simone4@gmail.com>
2023-03-28 16:52:57 +0200

    bug found

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default {
+    data(){
+        return{
+            
+        }
+    },
+
+    props:{
+        Card: Object,
+    }
+}
+</script>
+
+<template> 
+    <div class="card">
+        <div class="img-container">
+            <img :src="Card" alt="cardImg">
+        </div>
+        <div class="description">
+            
+        </div>
+    </div>
+</template> 
+
+<style lang="scss" scoped>
+    
+    .card{
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        color: white;
+
+        width: calc(100% / 6 - 10px / 6 * 5);
+
+    }
+
+</style>
