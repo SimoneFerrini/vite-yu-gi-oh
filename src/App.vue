7e7fe632deb293663af6f7feaf413d719e2d284e
SimoneFerrini <ferrini.simone4@gmail.com>
2023-03-28 14:42:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Header from "./components/AppHeader.vue";
+
+export default{
+  data(){
+    return{
+
+    }
+  },
+
+  components:{
+    Header,
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
